4b25eb78d09146633901edcbfe0b86c243abc4fc
ovsiannikov <ollsanek@gmail.com>
2016-11-02 16:14:49 +0200

    Create homework_ovsiannikov_2.2.py

---
+++ homework_ovsiannikov_2.2.py
@@ -0,0 +1,7 @@
+import math
+input_value=float(input())
+print('sinus    of '   ,input_value,' is: ',math.sin(input_value))
+print('cosinus  of ' ,input_value,  ' is: ',math.cos(input_value))
+print('tangens  of ' ,input_value,  ' is: ',math.tan(input_value))
+print('cotanges of ',input_value,   ' is: ', 1/math.tan(input_value))
+
